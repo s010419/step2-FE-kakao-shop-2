ebbac4f60fe2c6ef649f6a59f1766f005cfa8e79
Sang Min Park <125769061+s010419@users.noreply.github.com>
2023-08-10 02:33:04 +0900

    Update Card.jsx

--- src/components/atoms/Card.jsx
+++ src/components/atoms/Card.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-// import "../../styles/atoms/Card.css"
 
 const Card = ({ to, children }) => {
   // to 이동할 경로
@@ -7,7 +6,7 @@ const Card = ({ to, children }) => {
   return (
     // Link element react 개발시 꼭 사용!
     // Link : react 상에서 페이지 변화하는 형태로 작동하지 않음, UX적으로도 부드럽게 작동
-    <Link className="card" to={to}>
+    <Link className="card bg-gray-100 rounded-lg p-2 m-2 w-250" to={to}>
       {children}
     </Link>
   );
