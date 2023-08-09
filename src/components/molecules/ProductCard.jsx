2bb9bb372188623170073fc4c650074ef4212941
Sang Min Park <125769061+s010419@users.noreply.github.com>
2023-08-10 01:48:01 +0900

    Update ProductCard.jsx

--- src/components/molecules/ProductCard.jsx
+++ src/components/molecules/ProductCard.jsx
@@ -6,16 +6,21 @@ const staticServerUrl = process.env.REACT_APP_PATH || "";
 
 const ProductCard = ({ product }) => {
   return (
-    <Card to={staticServerUrl + `/product/${product.id}`}>
-      <Photo
-        src={product.image}
-        alt={product.productName}
-        className="rounded-lg"
-      />
+    <Card
+      className="bg-gray-100 rounded-lg p-2 m-2 w-250"
+      to={staticServerUrl + `/product/${product.id}`}
+    >
+      <div className="h-240">
+        <Photo
+          src={product.image}
+          alt={product.productName}
+          className="rounded-lg object-cover h-full w-full"
+        />
+      </div>
       <div className="text-sm font-medium text-gray-500">
         {product.productName}
       </div>
-      <div>{comma(product.price)}원</div>
+      <div className="text-sm font-bold">{comma(product.price)}원</div>
     </Card>
   );
 };
